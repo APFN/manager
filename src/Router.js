6c0e6eeab789293d5f1849895087a61b00ca435f
APFN <alvarodenegreiros@gmail.com>
2019-02-21 11:11:48 -0300

    final lecture 130

--- src/Router.js
+++ src/Router.js
@@ -6,22 +6,34 @@ import EmployeeList from './components/Employee';
 const RouterComponent = () => {
     return(
         <Router>
-            <Scene key= "root">
-                <Scene 
-                    key="login" 
-                    component={LoginForm} 
-                    title="Please Login"
-                    initial
-                /> 
-                <Scene 
-                    key="employeeList" 
-                    component={EmployeeList} 
-                    title="Employees"
-                />                
+            <Scene key= "root"hideNavBar>
+                <Scene key= "auth">
+                    <Scene 
+                        key="login" 
+                        component={LoginForm} 
+                        title="Please Login"
+                        initial
+                        stitleStyle={styles.navigationBarTitleStyle}
+                    /> 
+                </Scene> 
+                <Scene key= "main">
+                    <Scene 
+                        key="employeeList" 
+                        component={EmployeeList} 
+                        title="Employees"
+                        stitleStyle={styles.navigationBarTitleStyle}
+                    />   
+                </Scene>         
             </Scene>
         </Router>
 
     );
 };
 
+const styles = {
+    navigationBarTitleStyle: {
+        flex: 1,
+        textAlign: 'center'
+    }
+}
 export default RouterComponent;
