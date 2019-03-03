e36063d7ec5f9ed3d952bb0b9b12fe47469cc905
APFN <alvarodenegreiros@gmail.com>
2019-03-03 15:36:45 -0300

    final lecture 156

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import {View, Text, Picker } from 'react-native';
 import {CardSection, Card, Button } from './common';
 import { connect } from 'react-redux';
-import { employeeUpdate} from '../actions';
+import { employeeUpdate, employeeSave} from '../actions';
 import EmployeeForm from  './EmployeeForm';
 
 class EmployeeEdit extends Component{
@@ -16,6 +16,7 @@ class EmployeeEdit extends Component{
   }
   onButtonPress(){
     const { name, phone, shift } = this.props;
+    this.props.employeeSave({name, phone, shift, uid: this.  props.employee.uid})
   }
 
     render(){
@@ -41,4 +42,7 @@ const mapStateToProps = (state) => {
     return { name, phone, shift };
   }
   
-  export default connect( mapStateToProps, { employeeUpdate })(EmployeeEdit);
+  export default connect( mapStateToProps, { 
+      employeeUpdate,
+      employeeSave
+    })(EmployeeEdit);
